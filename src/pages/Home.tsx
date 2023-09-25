c0c84e8da125810e8bed8f6c3e77f05dee578895
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-25 15:30:31 +0100

    7 files modified, 1 deleted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,59 +1,32 @@
 import React, { useState } from 'react';
 import './Home.css';
 import { useUser } from '../UserContext';
+import { Link } from 'react-router-dom';
 
 const Home = ({ handleUserButton }) => {
+  const users = [
+    { value: 'eddie', text: 'Eddie' },
+    { value: 'lucia', text: 'Lucia' },
+    { value: 'gma', text: 'Gma' },
+    { value: 'sarah', text: 'Sarah' },
+    { value: 'lino', text: 'Lino' },
+    { value: 'elisa', text: 'Elisa' },
+  ];
+
   return (
     <div>
       <h1>Les Taillis</h1>
       <div className="buttonContainer">
-        <div className="buttonRow">
-          <button
-            id="homeButton"
-            className="eddieButton"
-            onClick={() => handleUserButton('eddie', 'lightblue')}
-          >
-            Eddie
-          </button>
-          <button
-            id="homeButton"
-            className="luciaButton"
-            onClick={() => handleUserButton('lucia', 'lightskyblue')}
-          >
-            Lucia
-          </button>
-        </div>
-        <div className="buttonRow">
-          <button
-            id="homeButton"
-            className="gmaButton"
-            onClick={() => handleUserButton('gma', 'lightgreen')}
-          >
-            Gma
-          </button>
-          <button
-            id="homeButton"
-            className="sarahButton"
-            onClick={() => handleUserButton('sarah', 'lightpink')}
-          >
-            Sarah
-          </button>
-        </div>
-        <div className="buttonRow">
-          <button
-            id="homeButton"
-            className="linoButton"
-            onClick={() => handleUserButton('lino', 'lightseagreen')}
-          >
-            Lino
-          </button>
-          <button
-            id="homeButton"
-            className="elisaButton"
-            onClick={() => handleUserButton('elisa', 'lightsalmon')}
-          >
-            Elisa
-          </button>
+        <div className="buttonGrid">
+          {users.map(({ value, text }) => (
+            <Link
+              key={value}
+              to={`/area`}
+              className={`homeButton ${value}Button`}
+            >
+              {text}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
