b6a9aea9b73bc67971866801a275168c9a01c085
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-26 14:53:38 +0100

    4 files modified, 1 added

--- src/components/Tasks/UpdateTask.tsx
+++ src/components/Tasks/UpdateTask.tsx
@@ -2,18 +2,41 @@ import React, { useState, useEffect } from 'react';
 import './UpdateTask.css';
 
 const UpdateTask = () => {
+  const [newUpdate, setNewUpdate] = useState();
+
+  const handleUpdateTask = () => {
+    if (newUpdate.trim() !== '') {
+      addUpdate({ update: newUpdate });
+      // setNewItem('');
+      // setNewDescription('');
+    }
+  };
+
   return (
     <div className="updateTaskContainer">
       <div className="updateTaskTitle">Updates</div>
-      <input
-        type="text"
-        className="inputTaskUpdate"
-        placeholder="Add an update..."
-        // value={newItem}
-        // onChange={(e) => setNewItem(e.target.value)}
-      />
+      <div className="updateTaskAddUpdate">
+        <input
+          type="text"
+          className="inputTaskUpdate"
+          placeholder="Add an update..."
+          value={newUpdate}
+          // onChange={(e) => setNewUpdate(e.target.value)}
+        />
+        <button
+          className="updateTaskAddUpdateButton"
+          onClick={handleUpdateTask}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 };
 
 export default UpdateTask;
+
+// when type update and press enter/add button
+// update should be stored in firestore database
+// re render the edit task screen
+// list the updates for that task on screen
