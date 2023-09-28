5bb92ee64e7d2f3739757a74571451ca6f106cd6
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-28 15:53:21 +0100

    4 files modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,27 +7,47 @@ const Home = () => {
   const { setSelectedUser, users } = useUser();
   const [clickedUser, setClickedUser] = useState(null);
   const navigate = useNavigate();
+  const [buttonPosition, setButtonPosition] = useState({ top: 0, left: 0 }); // Track button position
 
   const handleUserClick = (user) => {
     setSelectedUser(user.value);
     setClickedUser(user.value);
+
+    // Get the button's position before moving it
+    const button = document.querySelector(`.${user.value}Button`);
+    const buttonRect = button.getBoundingClientRect();
+
+    // Calculate the new position (center of the screen)
+    const centerX = window.innerWidth / 2 - buttonRect.width / 2;
+    const centerY = window.innerHeight / 2 - buttonRect.height / 2;
+
+    // Update the button's position
+    setButtonPosition({ top: centerY, left: centerX });
+
     setTimeout(() => {
       navigate('/area');
     }, 2000);
   };
 
+  console.log('clicked user', clickedUser);
   return (
     <div className="homeContainer">
       {clickedUser ? null : <h1>Les Taillis</h1>}
-      <div className={`buttonContainer ${clickedUser ? 'hidden' : ''}`}>
-        <div className="buttonGrid">
+      <div className={'buttonContainer'}>
+        <div className={'buttonGrid'}>
           {users.map((user) => (
             <Link
               key={user.value}
               // to={`/area`}
-              className={`homeButton ${user.value}Button`}
+              className={`homeButton ${user.value}Button ${
+                !clickedUser
+                  ? ''
+                  : clickedUser === user.value
+                  ? 'centered'
+                  : `hidden`
+              }`}
               onClick={() => handleUserClick(user)}
-              style={{ backgroundColor: user.color }}
+              style={{ backgroundColor: user.color, ...buttonPosition }}
             >
               {user.text}
             </Link>
