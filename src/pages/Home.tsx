b88d12e815fffc4e30fa6e22c8471299f9df6ba9
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-28 15:07:12 +0100

    1 file modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import { useUser } from '../UserContext';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const { setSelectedUser, users } = useUser();
+  const [clickedUser, setClickedUser] = useState(null);
+  const navigate = useNavigate();
 
-  const handleUserClick = (user: User) => {
+  const handleUserClick = (user) => {
     setSelectedUser(user.value);
+    setClickedUser(user.value);
+    setTimeout(() => {
+      navigate('/area');
+    }, 2000);
   };
 
   return (
     <div className="homeContainer">
-      <h1>Les Taillis</h1>
-      <div className="buttonContainer">
+      {clickedUser ? null : <h1>Les Taillis</h1>}
+      <div className={`buttonContainer ${clickedUser ? 'hidden' : ''}`}>
         <div className="buttonGrid">
           {users.map((user) => (
             <Link
               key={user.value}
-              to={`/area`}
+              // to={`/area`}
               className={`homeButton ${user.value}Button`}
               onClick={() => handleUserClick(user)}
-              style={{ backgroundColor: user.color }} // Set the background color
+              style={{ backgroundColor: user.color }}
             >
               {user.text}
             </Link>
