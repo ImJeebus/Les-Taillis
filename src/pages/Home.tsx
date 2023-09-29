982b9f0eac8812ae663647f3a3a25b6f83e66559
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-29 09:58:25 +0100

    1 file modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ const Home = () => {
     return positions;
   };
 
-  // Set the initial button positions when the component mounts
+  // Set initial button positions
   useEffect(() => {
     const initialPositions = calculateInitialButtonPositions();
     setButtonPositions(initialPositions);
@@ -46,7 +46,7 @@ const Home = () => {
       [user.value]: { top: centerY, left: centerX },
     });
 
-    // Navigate after a delay
+    // Navigate after delay
     setTimeout(() => {
       navigate('/area');
     }, 1500);
