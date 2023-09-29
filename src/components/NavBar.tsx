a40b8fc59a665141cf31691937fcbd4f5a498a29
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-09-29 16:42:49 +0100

    3 files modified

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,6 +14,11 @@ const NavBar = () => {
           <Profile />
         </div>
       )}
+      {location.pathname !== '/' && location.pathname !== '/area' && (
+        <Link to="/area" className="navBarArea">
+          <BiBorderNone />
+        </Link>
+      )}
     </div>
   );
 };
