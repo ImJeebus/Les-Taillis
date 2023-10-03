d3e3fa2dad47a97b5d8ff92f45066720bd8323f6
ImJeebus <94166558+ImJeebus@users.noreply.github.com>
2023-10-03 16:32:47 +0100

    1 file modified

--- src/UserReducer.tsx
+++ src/UserReducer.tsx
@@ -1,2 +1,24 @@
 import { useReducer } from 'react';
 
+const initialState = {
+  username: '',
+  text: '',
+  color: '',
+};
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'UPDATE_USERNAME':
+      return { ...state, name: action.payload };
+    case 'UPDATE_TEXT':
+      return { ...state, text: action.payload };
+    case 'UPDATE_COLOR':
+      return { ...state, color: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function useUserReducer() {
+  return useReducer(userReducer, initialState);
+}
